0bcacef731b5c8381e3d816fa9339008e2f026e9
Marianina <ninagutierreza93@gmail.com>
2021-04-28 19:40:13 -0400

    Adding add function

---
+++ practice1.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a+b
+
+def main():
+    a = int(input("Enter a:"))
+    b = int(input('Enter b:'))
+
+    print(add(a,b))
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
